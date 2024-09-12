8796d99073bd1bcdfd53727cf52ac4ae4c53280b
PARK JEEAE <msp827@naver.com>
2024-09-12 18:28:27 +0900

    Feat: 회원가입 페이지
    - 테이블명 변경: USER -> USERS

--- dugout/src/main/java/com/mycompany/dugout/controller/UserController.java
+++ dugout/src/main/java/com/mycompany/dugout/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.mycompany.dugout.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.dugout.dto.UserDto;
+import com.mycompany.dugout.service.UserService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,28 +15,43 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequestMapping("user")
 public class UserController {
+	@Autowired
+	UserService userService;
+	
 	@RequestMapping("/login")
 	public String login() {
-		log.info("실행");
 		return "user/login";
 	}
 	
-	@RequestMapping("/joinForm")
+	@GetMapping("/joinForm")
 	public String joinForm() {
-		log.info("실행");
 		return "user/joinForm";
 	}
 	
+	@PostMapping("join")
+	public String join(UserDto form) {
+		UserDto user = new UserDto();
+		
+		user.setUserId(form.getUserId());
+		user.setUserPassword(form.getUserPassword());
+		user.setUserName(form.getUserName());
+		user.setUserPhone(form.getUserPhone());
+		user.setUserAddress(form.getUserAddress());
+		user.setUserEmail(form.getUserEmail());
+		user.setUserRole("user");
+		user.setUserActive(1);
+
+		userService.join(user);
+		return "redirect:/user/login";
+	}
 	
 	@RequestMapping("/findId")
 	public String findId() {
-		log.info("실행");
 		return "user/findId";
 	}
 	
 	@RequestMapping("/findPassword")
 	public String findPassword() {
-		log.info("실행");
 		return "user/findPassword";
 	}
 }
--- dugout/src/main/java/com/mycompany/dugout/service/UserService.java
+++ dugout/src/main/java/com/mycompany/dugout/service/UserService.java
@@ -1,8 +1,18 @@
 package com.mycompany.dugout.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mycompany.dugout.dao.UserDao;
+import com.mycompany.dugout.dto.UserDto;
 
 @Service
 public class UserService {
+	@Autowired
+	private UserDao userDao;
+
+	public void join(UserDto user) {
+		userDao.join(user);
+	}
 
 }
