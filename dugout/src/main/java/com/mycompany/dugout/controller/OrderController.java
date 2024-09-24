83e3f15d1ff58200ad0a75ae78e50e21a1a8d580
soob511 <asdfls@naver.com>
2024-09-24 09:33:33 +0900

    Feat: 결제페이지 조회 추가

--- dugout/src/main/java/com/mycompany/dugout/controller/OrderController.java
+++ dugout/src/main/java/com/mycompany/dugout/controller/OrderController.java
@@ -1,11 +1,22 @@
 package com.mycompany.dugout.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mycompany.dugout.dto.PayItemDto;
+import com.mycompany.dugout.dto.UserDto;
+import com.mycompany.dugout.security.UserDetail;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,16 +30,31 @@ public class OrderController {
 	
 		return "mypage/orderList";
 	}
+	
+	@GetMapping("/payment")
+	public String payment(Model model,Authentication authentication,HttpSession session) {
+		UserDetail userDetail = (UserDetail) authentication.getPrincipal();
+		UserDto user = userDetail.getUser();
+		model.addAttribute("user",user);
+	
+		
+		return "pay/payment";
+	}
 
 	@ResponseBody
 	@PostMapping("/orderItem")
-	public boolean orderItem(int[] orderList,HttpSession session) {
+	public boolean orderItem(@RequestBody Map<String, Object> map,HttpSession session) {
+		
+		List<PayItemDto> orderList = (List<PayItemDto>) map.get("orderList");
+	    int totalPrice = (int) map.get("totalPrice");
+		
 		
 		if(orderList==null) {
 			return false;
 		}
+		
 		session.setAttribute("orderList", orderList);
+		session.setAttribute("totalPrice", totalPrice);
 		return true;
 	}
-	
 }
