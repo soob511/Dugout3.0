81d5dd13d68e6fa14e4ac14c4400c4ae873f510b
PARK JEEAE <msp827@naver.com>
2024-09-29 14:34:24 +0900

    Update: 접근권한 페이지 CSS 수정

--- dugout/src/main/java/com/mycompany/dugout/controller/NoticeController.java
+++ dugout/src/main/java/com/mycompany/dugout/controller/NoticeController.java
@@ -52,6 +52,7 @@ public class NoticeController {
 		return "notice/noticeDetail";
 	}
 	
+	@Secured("ROLE_ADMIN")
 	@GetMapping("/writeNoticeForm")
 	public String writeNoticeForm() {
 		return "notice/noticeForm";
@@ -76,6 +77,7 @@ public class NoticeController {
 		return "notice/updateNoticeForm";
 	}
 	
+	@Secured("ROLE_ADMIN")
 	@PostMapping("/updateNotice")
 	public String updateNotice(UpdateNoticeDto form) {
 		noticeService.updateNotice(form);		
